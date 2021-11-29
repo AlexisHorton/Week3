c50393a000e0aea4c5dcda5b48eceb8304b65329
alexis <alexisd.horton@gmail.com>
2021-11-29 12:00:32 -0500

    Monday

--- ArrayReview/Program.cs
+++ ArrayReview/Program.cs
@@ -4,9 +4,103 @@ namespace ArrayReview
 {
     class Program
     {
+        // This funcion takes an array and returns a single integer (a scalar).
+        static int Average(int[] nums)
+        {       //sum
+            int accum = 0;
+            foreach (int next in nums)
+            {
+                accum += next;
+            }
+            return accum / nums.Length;
+        }
+
+        //This function takes a count, creates an array of that size, and fills it with numbers
+        static int[] FillArray(int count)
+        {
+            int[] myarray = new int[count];
+            //ex: An array of size 5 we will fill with 0, 1, 2, 3, 4.
+            // i * 2 will give even output= 0, 2, 4, 6, 8 
+
+            for (int i = 0; i < myarray.Length; i++)
+            {
+                myarray[i] = i * 2;
+            }
+
+            return myarray;
+        }
+
+        //This function prints out what's in an array
+        static void PrintArray(int[] thearray)
+        {
+            Console.WriteLine("\nHere's what is in the array");
+            for (int i = 0; i < thearray.Length; i++)
+            {
+                Console.WriteLine($"Index {i} contains {thearray[i]}");
+            }
+        }
+
+
+        //This function increments each item in the array
+        //For ex, pass in [5, 10, 15, 20],
+        //this function will modify that array, changing it to:
+        // [6, 11, 16, 21]
+        //When we pass an array into a function, 
+        // the function has the actual original array, NOT a copy.
+        // So any changes we make are changing the original array.
+        static void ChangeArray(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = nums[i] + 1;
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Create an array!");
+            int[] sizes = new int[] { 5, 10, 15, 20 };
+
+            Console.WriteLine("\nHere's what is in the array");
+            for (int i = 0; i < sizes.Length; i++)
+            {
+                Console.WriteLine($"Index {i} contains {sizes[i]}");
+            }
+
+            Console.WriteLine("\nLet's count downn from the end to the beginning!");
+            for (int i = sizes.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine($"Index {i} contains {sizes[i]}");
+            }
+
+            Console.WriteLine("\nLet's use a foreach loop");
+            foreach (int item in sizes)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\nCreate another array without initialization");
+            int[] numbers = new int[4];
+            numbers[0] = 10;
+            numbers[1] = 20;
+            numbers[2] = 30;
+            numbers[3] = 40;
+
+            //Console.WriteLine(numbers[2]);
+
+            int avg = Average(numbers);
+            Console.WriteLine($"The average is {avg}");
+
+            Console.WriteLine($"The average is {Average(numbers)}"); //Same output as above
+
+            Console.WriteLine("\nLet's call our FillArray method to create an array!");
+            int[] morenums = FillArray(5);
+            PrintArray(morenums);
+
+            Console.WriteLine("\nCreate an array and pass it to our ChangeArray method.");
+            int[] tochange = new int[] { 5, 10, 15, 20 };
+            ChangeArray(tochange);
+            PrintArray(tochange);
         }
     }
 }
---
+++ ListReview/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListReview
+{
+    class Program
+    {
+
+        //This function takes a list and returns a single integer
+        static int Average(List<int> nums)
+        {
+            int accum = 0;
+            foreach (int next in nums)
+            {
+                accum += next;
+            }
+            return accum / nums.Count;
+        }
+
+
+        //This function takes a single number and creates and fiils a list
+        static List<int> FillList(int count)
+        {
+            List<int> mylist = new List<int>();
+            for (int i = 0; i < count; i++)
+            {
+                mylist.Add(i * 2);
+            }
+            return mylist;
+        }
+
+        //This function prints out a list
+        static void PrintList(List<int> mylist)
+        {
+            Console.WriteLine($"\nThis list has {mylist.Count}. Here's what's in the list:");
+            for (int i = 0; i < mylist.Count; i++)
+            {
+                Console.WriteLine($"Index {i} contains {mylist[i]}.");
+            }
+        }
+
+        //This function increments each item in the list by 1
+        static void ChangeList(List<int> nums)
+        {
+            for (int i = 0; i < nums.Count; i++)
+            {
+                nums[i] = nums[i] + 1;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Create a list!");
+            List<int> sizes = new List<int>() { 5, 10, 15, 20 };
+
+            Console.WriteLine("Let's get the average!");
+            Console.WriteLine(Average(sizes));
+
+            Console.WriteLine("\nLet's fill a list and print it!");
+            List<int> thelist = FillList(6);
+            PrintList(thelist);
+            //PrintList(FillList(6)); // gives same output ^
+
+            Console.WriteLine("\nLet's change the items in the list and print it");
+            ChangeList(thelist);
+            PrintList(thelist);
+
+            Console.WriteLine("\nLet's reverse this thing!!");
+            thelist.Reverse();
+            PrintList(thelist);
+
+            Console.WriteLine("\nLet's manually change a couple items");
+            thelist[2] = 50;
+            thelist[4] = 100;
+            thelist.Add(101);
+            thelist.RemoveAt(3);
+            PrintList(thelist);
+
+            Console.WriteLine("\nLet's sort the list!");
+            thelist.Sort(); // sorts the list in order from numerical value
+            PrintList(thelist);
+
+            Console.WriteLine("\nLet's remove 50 from it!");
+            thelist.Remove(50);
+            PrintList(thelist);
+
+        }
+    }
+}
